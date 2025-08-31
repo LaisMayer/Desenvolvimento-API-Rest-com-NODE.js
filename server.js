1bbd2b454fb32e0dff656399d05b3e00af7ca315
Lais Mayer <20233007175@estudantes.ifpr.edu.br>
2025-08-31 10:45:25 -0300

    finalizado

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import path from "path";
 import router from "./routes/router.js";
-import logger from "./middleware/logger.middleware.js";
+import logger from "./middleware/middleware.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express();
+const app = express(); 
 app.use(express.json());
 app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
